bd3e4b6c9d30727a5af131545e0dee9d4eb755e5
javlintor <jlinarestorres2@gmail.com>
2026-04-07 08:18:57 +0200

    update dotfiles

--- tmux_ifco.sh
+++ tmux_ifco.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SESS="ifco"
-DIRS=("ifco-digital-semantic-layer" "ifco-digital-data-apps")
+SESS=ifco
+DIRS=(ifco-digital-semantic-layer ifco-digital-data-apps daily_picker_wheel)
 
 tmux has-session -t $SESS 2>/dev/null
 
