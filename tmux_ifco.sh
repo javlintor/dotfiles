3c71faae3b75ffebc5641de9438a6cd319488a4d
javierli-ifco <javier.linares@ifco.com>
2026-06-22 17:00:08 +0200

    tmux management based on sessions instead of windows

--- tmux_ifco.sh
+++ tmux_ifco.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-SESS=ifco
-DIRS=(ifco-digital-semantic-layer ifco-digital-data-apps)
+DIRS=(ifco-digital-semantic-layer ifco-digital-data-apps dotfiles)
 
-tmux has-session -t $SESS 2>/dev/null
+for dir in ${DIRS[@]}; do
+	tmux has-session -t $dir 2>/dev/null
+	if [ $? != 0 ]; then
+		# window 1: nvim
+		tmux new-session -ds $dir -c ~/$dir -n nvim
+		tmux send-keys -t $dir:nvim "nvim" C-m
+		# window 2: claude
+		tmux new-window -t $dir -c ~/$dir -n claude
+		tmux send-keys -t $dir:claude "claude" C-m
+	fi
+done
 
-if [ $? != 0 ]; then
-	tmux new-session -ds $SESS
-	for i in ${!DIRS[@]}; do
-		dir=${DIRS[$i]}
-		if [[ $i -eq 0 ]]; then
-			# tmux index must start in 1
-			tmux rename-window -t $SESS:1 $dir
-		else
-			tmux new-window -t $SESS:$((i+1)) -n $dir
-		fi
-		tmux send-keys -t $SESS:$dir "cd ~/$dir" C-m
-		tmux send-keys -t $SESS:$dir "nvim" C-m
-	done
-
-
-fi
-
-tmux attach-session -t $SESS:1
+tmux attach-session -t ${DIRS[0]}
