2c92a3de9662d2235bb9b1977c8fceac8e7b9b54
javierli-ifco <javier.linares@ifco.com>
2026-09-07 14:17:38 +0200

    add ifco-digital-web-apps to tmux ifco

--- tmux_ifco.sh
+++ tmux_ifco.sh
@@ -5,6 +5,7 @@ DIRS=(
 	ifco-digital-semantic-layer:ifco-digital-semantic-layer
 	ifco-digital-data-apps:ifco-digital-data-apps
 	ifco-digital-ops-optimizer-ingestion:ifco-digital-ops-optimizer-ingestion
+	ifco-digital-web-apps:ifco-digital-web-apps
 	dotfiles:dotfiles
 	nvim:.config/nvim
 )
